5d09645d15ccd6974d838e95f8760f7db7d1d0d3
Rahul R <rahul.raichur99@gmail.com>
2022-08-24 00:21:52 +0530

    Unique Elements

---
+++ UniqueElements.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    class UniqueElements
+    {
+        public static void Main()
+
+
+        {
+            int[] items = { 991, 998, 1335, 2312, 2337, 1335, 992, 2312,993, 996 };
+            int n = items.Length;
+
+            Console.WriteLine("Unique array elements are: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                bool Same = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (items[i] == items[j])
+                    {
+                        Same = true;
+                        break;
+                    }
+                }
+
+                if (!Same)
+                {
+                    Console.WriteLine(items[i]);
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
