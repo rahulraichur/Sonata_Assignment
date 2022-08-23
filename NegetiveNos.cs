f49a74c08c5383fa33dbd4f6e3b5b311473b72ad
Rahul R <rahul.raichur99@gmail.com>
2022-08-24 00:29:59 +0530

    Negetive Nos

---
+++ NegetiveNos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class NegetiveNos
+    {
+        public static void Main()
+        {
+            int[] arr = { 39, -4, 7, 55, -32, -22 };
+            int i, n;
+            n = arr.Length;
+            for (i = 1; i < n; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    Console.Write($"{arr[i]}");
+                }
+
+            }
+        }
+    }
+}
