f5cc90d630e55541538b7bd58bd0a29c75f874cc
rahulraichur <92712657+rahulraichur@users.noreply.github.com>
2022-08-25 21:58:54 +0530

    Add files via upload

---
+++ LargestPrimeFactor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class LargestPrimeFactor
+    {
+        static long MaxPrime(long num)
+        {
+            long CurrMaxPrime = -1;
+
+            if (num % 2 == 0)
+            {
+                CurrMaxPrime = 2;
+                while (num % 2 == 0)
+                {
+                    num = num / 2;
+                }
+            }
+
+            for (long i = 3; i <= Math.Sqrt(num); i += 2)
+            {
+                while (num % i == 0)
+                {
+                    CurrMaxPrime = i;
+                    num = num / i;
+                }
+            }
+
+            if (num > 2)
+                CurrMaxPrime = num;
+
+            return CurrMaxPrime;
+        }
+
+        static void Main(string[] args)
+        {
+            long x, y, z;
+            x = 9;
+            y = 180;
+            z = 212;
+
+            Console.WriteLine("Largest prime factor of " + x
+                               + " is: " + MaxPrime(x));
+            Console.WriteLine("Largest prime factor of " + y
+                               + " is: " + MaxPrime(y));
+            Console.WriteLine("Largest prime factor of " + z
+                               + " is: " + MaxPrime(z));
+        }
+    }
+}
+
+
